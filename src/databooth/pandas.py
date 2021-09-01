a6cf4a1fefbe858063976acd0e3889cbebbeba75
Michael Booth <michael@databooth.com.au>
2021-09-02 06:32:03 +1000

    Clean-up for public

--- src/databooth/pandas.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd 
-
-def read_google_sheet(sheet_url):
-    sheet_url = sheet_url.replace(‘/edit#gid=’, ‘/export?format=csv&gid=’)
-    return pd.read_csv(sheet_url)
-
-# See: https://towardsdatascience.com/read-data-from-google-sheets-into-pandas-without-the-google-sheets-api-5c468536550
